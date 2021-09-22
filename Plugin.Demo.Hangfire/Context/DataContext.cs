a4369f8385f6711731f394cf6de17e9a6b36d6d8
1039270526@qq.com <1039270526@qq.com>
2021-09-22 16:53:13 +0800

    添加数据库上下文， 生成数据表

---
+++ Plugin.Demo.Hangfire/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Plugin.Demo.Hangfire.Models;
+
+namespace Plugin.Demo.Hangfire.Context
+{
+    /// <summary>
+    /// 数据库上下文类
+    /// </summary>
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<JobConfig> JobConfig { get; set; }
+    }
+}
---
+++ Plugin.Demo.Hangfire/Models/JobConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Plugin.Demo.Hangfire.Models
+{
+    /// <summary>
+    /// Hangfire 定时任务配置
+    /// </summary>
+    public class JobConfig
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Corn表达式
+        /// </summary>
+        public string Corn { get; set; }
+    }
+}
