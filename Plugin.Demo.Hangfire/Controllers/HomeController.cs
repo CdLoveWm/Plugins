c075611167e0bb85fd313e33ed59eccf2de47513
1039270526@qq.com <1039270526@qq.com>
2021-09-23 13:54:14 +0800

    Cron 表达式解析 返回最近运行的时间列表

--- Plugin.Demo.Hangfire/Controllers/HomeController.cs
+++ Plugin.Demo.Hangfire/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Hangfire;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NCrontab;
 using Plugin.Demo.Hangfire.Jobs.Interfaces;
 using Plugin.Demo.Hangfire.Models;
 using System;
@@ -45,6 +46,36 @@ namespace Plugin.Demo.Hangfire.Controllers
             // 更新Hangfire的Corn表达式
             RecurringJob.AddOrUpdate<ITestJob>(it => it.Excute(), cron, TimeZoneInfo.Local);
             return true;
+        }
+
+        /// <summary>
+        /// Cron 表达式解析
+        /// 返回最近运行的时间列表
+        /// </summary>
+        /// <param name="cron"></param>
+        /// <returns></returns>
+        public List<string> CronParse(string cron)
+        {
+            if (string.IsNullOrWhiteSpace(cron))
+                return null;
+            try
+            {
+                var parser = CrontabSchedule.Parse(cron);
+                List<string> dateList = new List<string>();
+                DateTime startTime = DateTime.Now;
+                DateTime endTime = DateTime.MaxValue;
+                for (int i = 0; i < 5; i++)
+                {
+                    startTime = parser.GetNextOccurrence(startTime, endTime);
+                    dateList.Add(startTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                }
+                return dateList;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
